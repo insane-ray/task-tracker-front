65c5f8b15dc185eac7cc113fad6239423010cd10
Roman Zubkov <zubkov.r.v@effective-group.ru>
2020-05-21 06:01:12 +0300

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FacadeModule } from "./modules/facade/facade.module";
+import { PortalModule } from "./modules/portal/portal.module";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import { ForbiddenComponent } from './pages/forbidden/forbidden.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForbiddenComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    FacadeModule,
+    PortalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/facade/facade.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FacadeRoutingModule } from './facade-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FacadeRoutingModule
+  ]
+})
+export class FacadeModule { }
